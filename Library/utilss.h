8c2efea6814e3e988ff94b2039b728dd18a67846
Yasmeen Ahmed <47220103+yasmeenahmed94@users.noreply.github.com>
2019-03-27 20:10:17 +0200

    Update utilss.h

--- Library/utilss.h
+++ Library/utilss.h
@@ -14,5 +14,11 @@
 #define toggle_bit(reg,bitnum)  reg=reg^(1<<bitnum)
 #define get_bit(reg,bitnum) ((reg&(1<<bitnum))>>bitnum)
 #define assign_bit(reg,bitnum,value) do{clr_bit(reg,bitnum);reg=reg|(value<<bitnum);}while(0)
+#define set_high_nibble(reg) reg=reg|0xf0
+#define clr_high_nibble(reg) reg=reg&0x0f
+#define set_low_nibble(reg) reg=reg|0x0f
+#define clr_low_nibble(reg) reg=reg&0xf0
+#define get_high_nibble(reg) ((reg&0xf0)>>4)
+#define get_low_nibble(reg)  (reg&0x0f)
 
 #endif /* UTILSS_H_ */
