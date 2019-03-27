edf29c8c91f8ac0d629cb7d3f9e4f9e5d3828f57
Yasmeen Ahmed <47220103+yasmeenahmed94@users.noreply.github.com>
2019-03-27 12:38:21 +0200

    Add files via upload

---
+++ HAL/LED Driver/led.h
@@ -0,0 +1,16 @@
+/*
+ * led.h
+ *
+ *  Created on: Mar 26, 2019
+ *      Author: Yasmeen
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+extern void LED_vidInit(void);
+extern void LED_vidTurnONLed(u8 Led_Index);
+extern void LED_vidTurnOffLed(u8 Led_Index);
+extern void LED_vidToggleLed(u8 Led_Index);
+extern void LED_vidFlashLed(u32 time,u8 Led_Index);
+#endif /* LED_H_ */
