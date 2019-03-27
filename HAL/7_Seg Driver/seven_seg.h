1d49195b785ab346cb35e49479adf4ce38f11a10
Yasmeen Ahmed <47220103+yasmeenahmed94@users.noreply.github.com>
2019-03-27 10:37:25 +0200

    Add files via upload

---
+++ HAL/7_Seg Driver/seven_seg.h
@@ -0,0 +1,16 @@
+/*
+ * seven_seg.h
+ *
+ *  Created on: Mar 26, 2019
+ *      Author: Yasmeen
+ */
+
+#ifndef SEVEN_SEG_H_
+#define SEVEN_SEG_H_
+
+extern void SEG_vidInit(void);
+extern void SEG_vidCountUp(u8 num);
+extern void SEG_vidDisplayNumZeroNine(u8 port ,u8 num);
+extern void SEG_vidCountUp(u8 num);
+extern void SEG_vidCountDown(u8 num);
+#endif /* SEVEN_SEG_H_ */
