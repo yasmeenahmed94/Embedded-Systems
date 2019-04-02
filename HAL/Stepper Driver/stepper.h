4cb11aff903cf32ef50d10a97bdc23c78e32782f
Yasmeen Ahmed <47220103+yasmeenahmed94@users.noreply.github.com>
2019-04-02 14:17:58 +0200

    Create stepper.h

---
+++ HAL/Stepper Driver/stepper.h
@@ -0,0 +1,16 @@
+/*
+ * stepper.h
+ *
+ *  Created on: Apr 2, 2019
+ *      Author: Yasmeen
+ */
+
+#ifndef STEPPER_H_
+#define STEPPER_H_
+
+extern void STEPPER_vidInit(void);
+extern void STEPPER_vidCW(void);
+extern void STEPPER_vidCCW(void);
+
+
+#endif /* STEPPER_H_ */
