262d6dea921eae6fa1fa305361cafdf996d3a8ee
Yasmeen Ahmed <47220103+yasmeenahmed94@users.noreply.github.com>
2019-04-06 14:20:25 +0200

    ADC Peripheral

---
+++ MCAL/ADC Driver/ADC_cfg.h
@@ -0,0 +1,21 @@
+/*
+ * ADC_cfg.h
+ *
+ *  Created on: Apr 5, 2019
+ *      Author: Yasmeen
+ */
+
+#ifndef ADC_CFG_H_
+#define ADC_CFG_H_
+
+adc_config configuration = {
+		AVCC,
+		left,
+		ADC0,
+		disable,
+		DF_2
+
+};
+
+
+#endif /* ADC_CFG_H_ */
